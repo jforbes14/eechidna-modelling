75529843cb4eb28f2b25ebfd0369f613466b78d1
jforbes14 <jefor3@student.monash.edu>
2019-03-05 09:08:44 +1100

    order divisions alphabetically to match spatial weights. added code to get spatial weights.

--- create-data.R
+++ create-data.R
@@ -148,4 +148,36 @@ small_df <- my_df %>%
     PublicHousing, MedianFamilyIncome, MedianHouseholdIncome, MedianPersonalIncome, MedianRent, 
     MedianLoanPay, Unemployed, LFParticipation))
 
+# Order electorates in alphabetical order to match spatial matrix
+
+small_df <- small_df %>% 
+  arrange(year, DivisionNm)
+
 save(small_df, file = "data/small_df.rda")
+
+# ------------------------------------------------------------------------------------
+
+# Get spatial weights
+library(rgeos)
+library(spdep)
+
+sF_16 <- sF_download(2016)
+sF_13 <- sF_download(2013)
+sF_10 <- sF_download(2010)
+sF_07 <- sF_download(2007)
+sF_04 <- sF_download(2004)
+sF_01 <- sF_download(2001)
+
+sp_weights_16 <- sp_weights_matrix(sF_16)
+sp_weights_13 <- sp_weights_matrix(sF_13)
+sp_weights_10 <- sp_weights_matrix(sF_10)
+sp_weights_07 <- sp_weights_matrix(sF_07)
+sp_weights_04 <- sp_weights_matrix(sF_04)
+sp_weights_01 <- sp_weights_matrix(sF_01)
+
+save(sp_weights_16, file = "data/sp_weights_16.rda")
+save(sp_weights_13, file = "data/sp_weights_13.rda")
+save(sp_weights_10, file = "data/sp_weights_10.rda")
+save(sp_weights_07, file = "data/sp_weights_07.rda")
+save(sp_weights_04, file = "data/sp_weights_04.rda")
+save(sp_weights_01, file = "data/sp_weights_01.rda")
