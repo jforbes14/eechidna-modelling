c07ec0a7b13f7c0632501e3bcc60804397c6adba
jforbes14 <jefor3@student.monash.edu>
2019-03-25 12:42:00 +1100

    updated models to FGLS

--- full-models.R
+++ full-models.R
@@ -1,43 +1,89 @@
 
-# Full models for each year
+# GLS model (FGLS)
+library(nlme)
+my_formula <- "LNP_Percent ~ ."
+my_data <- model_df %>% filter(year == "2016") %>% select(-c(DivisionNm, year))
+sp_weights <- sp_weights_16
 
-fmod16 <- errorsarlm(LNP_Percent ~ ., 
-  data=(model_df %>% filter(year == "2016") %>% dplyr::select(-c(year, DivisionNm))),
-  sp_weights_16, etype="error", method="eigen", interval=c(-1,0.999))
+my_fgls <- function(my_formula, my_data, sp_weights) {
+  # Spatial weights matrix
+  w_mat <- listw2mat(sp_weights)
+  
+  # Get OLS residuals
+  ols_model <- lm(my_formula, my_data)
+  my_res <- ols_model$residuals
+  
+  # Solve for rho
+  res_model <- lm(my_res ~ w_mat%*%my_res)
+  rho <- res_model$coefficients[2]
+  rho_df <- data.frame(estimate = rho, se = summary(res_model)$coefficients[2,2], p = summary(res_model)$coefficients[2,4])
+  
+  # Transform data for GLS
+  trans_mat <- diag(nrow(my_data)) - rho*w_mat
+  gls_data <- data.frame(LNP_Percent = trans_mat %*% my_data$LNP_Percent,
+    Intercept = trans_mat %*% rep(1,nrow(my_data))) %>% 
+    bind_cols(as.data.frame(trans_mat %*% as.matrix(my_data %>% select(-LNP_Percent))))
+  
+  # GLS model
+  my_formula <- formula(paste0(my_formula,  " - 1"))
+  gls_model <- gls(my_formula, gls_data)
+  gls_model$rho_df <- rho_df
+  gls_model$gls_data <- gls_data
+  gls_model$my_data <- my_data
+  
+  return(gls_model)
+}
 
-fmod13 <- errorsarlm(LNP_Percent ~ ., 
-  data=(model_df %>% filter(year == "2013") %>% dplyr::select(-c(year, DivisionNm))),
-  sp_weights_13, etype="error", method="eigen", interval=c(-1,0.999))
+# --------------------------------------------------------------------------------------------------
 
-fmod10 <- errorsarlm(LNP_Percent ~ ., 
-  data=(model_df %>% filter(year == "2010") %>% dplyr::select(-c(year, DivisionNm))),
-  sp_weights_10, etype="error", method="eigen", interval=c(-1,0.999))
+## Run full models for each year
+full_formula = "LNP_Percent ~ ."
 
-fmod07 <- errorsarlm(LNP_Percent ~ ., 
-  data=(model_df %>% filter(year == "2007") %>% dplyr::select(-c(year, DivisionNm))),
-  sp_weights_07, etype="error", method="eigen", interval=c(-1,0.999))
+# 2016
+glsmod16 <- my_fgls(full_formula, 
+  my_data = model_df %>% filter(year == "2016") %>% select(-c(DivisionNm, year)),
+  sp_weights = sp_weights_16)
 
-fmod04 <- errorsarlm(LNP_Percent ~ ., 
-  data=(model_df %>% filter(year == "2004") %>% dplyr::select(-c(year, DivisionNm))),
-  sp_weights_04, etype="error", method="eigen", interval=c(-1,0.999))
+# 2013
+glsmod13 <- my_fgls(full_formula, 
+  my_data = model_df %>% filter(year == "2013") %>% select(-c(DivisionNm, year)),
+  sp_weights = sp_weights_13)
 
-fmod01 <- errorsarlm(LNP_Percent ~ ., 
-  data=(model_df %>% filter(year == "2001") %>% dplyr::select(-c(year, DivisionNm))),
-  sp_weights_01, etype="error", method="eigen", interval=c(-1,0.999))
+# 2010
+glsmod10 <- my_fgls(full_formula, 
+  my_data = model_df %>% filter(year == "2010") %>% select(-c(DivisionNm, year)),
+  sp_weights = sp_weights_10)
+
+# 2007
+glsmod07 <- my_fgls(full_formula, 
+  my_data = model_df %>% filter(year == "2007") %>% select(-c(DivisionNm, year)),
+  sp_weights = sp_weights_07)
+
+# 2004
+glsmod04 <- my_fgls(full_formula, 
+  my_data = model_df %>% filter(year == "2004") %>% select(-c(DivisionNm, year)),
+  sp_weights = sp_weights_04)
+
+# 2001
+glsmod01 <- my_fgls(full_formula, 
+  my_data = model_df %>% filter(year == "2001") %>% select(-c(DivisionNm, year)),
+  sp_weights = sp_weights_01)
+
+# --------------------------------------------------------------------------------------------------
 
 ## Visualise coefficients and significance
 
 coef_df <- bind_rows(
-  data.frame(variable = fmod16$coefficients %>% names, estimate = fmod16$coefficients %>% unname, se = fmod16$rest.se %>% unname, p = summary(fmod16)$Coef[,4] %>% unname, year = 2016),
-  data.frame(variable = fmod13$coefficients %>% names, estimate = fmod13$coefficients %>% unname, se = fmod13$rest.se %>% unname, p = summary(fmod13)$Coef[,4] %>% unname, year = 2013),
-  data.frame(variable = fmod10$coefficients %>% names, estimate = fmod10$coefficients %>% unname, se = fmod10$rest.se %>% unname, p = summary(fmod10)$Coef[,4] %>% unname, year = 2010),
-  data.frame(variable = fmod07$coefficients %>% names, estimate = fmod07$coefficients %>% unname, se = fmod07$rest.se %>% unname, p = summary(fmod07)$Coef[,4] %>% unname, year = 2007),
-  data.frame(variable = fmod04$coefficients %>% names, estimate = fmod04$coefficients %>% unname, se = fmod04$rest.se %>% unname, p = summary(fmod04)$Coef[,4] %>% unname, year = 2004),
-  data.frame(variable = fmod01$coefficients %>% names, estimate = fmod01$coefficients %>% unname, se = fmod01$rest.se %>% unname, p = summary(fmod01)$Coef[,4] %>% unname, year = 2001)
+  data.frame(variable = glsmod16$coefficients %>% names, estimate = glsmod16$coefficients %>% unname, se = summary(glsmod16)$tTable[, "Std.Error"] %>% unname, p = summary(glsmod16)$tTable[, "p-value"] %>% unname, year = 2016),
+  data.frame(variable = glsmod13$coefficients %>% names, estimate = glsmod13$coefficients %>% unname, se = summary(glsmod13)$tTable[, "Std.Error"] %>% unname, p = summary(glsmod13)$tTable[, "p-value"] %>% unname, year = 2013),
+  data.frame(variable = glsmod10$coefficients %>% names, estimate = glsmod10$coefficients %>% unname, se = summary(glsmod10)$tTable[, "Std.Error"] %>% unname, p = summary(glsmod10)$tTable[, "p-value"] %>% unname, year = 2010),
+  data.frame(variable = glsmod07$coefficients %>% names, estimate = glsmod07$coefficients %>% unname, se = summary(glsmod07)$tTable[, "Std.Error"] %>% unname, p = summary(glsmod07)$tTable[, "p-value"] %>% unname, year = 2007),
+  data.frame(variable = glsmod04$coefficients %>% names, estimate = glsmod04$coefficients %>% unname, se = summary(glsmod04)$tTable[, "Std.Error"] %>% unname, p = summary(glsmod04)$tTable[, "p-value"] %>% unname, year = 2004),
+  data.frame(variable = glsmod01$coefficients %>% names, estimate = glsmod01$coefficients %>% unname, se = summary(glsmod01)$tTable[, "Std.Error"] %>% unname, p = summary(glsmod01)$tTable[, "p-value"] %>% unname, year = 2001)
 )
 
 coef_df %>% 
-  filter(variable != "(Intercept)") %>% 
+  filter(variable != "Intercept") %>% 
   mutate(upper95 = estimate + 1.96*se, lower95 = estimate - 1.96*se) %>% 
   ggplot() +
   geom_point(aes(x = year, y = estimate, col = factor(p < 0.05)), size = 3) +
@@ -46,58 +92,30 @@ coef_df %>%
   facet_wrap(~variable, scales = "free") +
   scale_color_manual(values = c("grey50", "black"))
 
-## Spatial correlation in residuals
-
-moran.test(fmod16$residuals, sp_weights_16)
-moran.test(fmod13$residuals, sp_weights_13)
-moran.test(fmod10$residuals, sp_weights_10)
-moran.test(fmod07$residuals, sp_weights_07)
-moran.test(fmod04$residuals, sp_weights_04)
-moran.test(fmod01$residuals, sp_weights_01)
-
-my_res <- fmod16$tary - fmod16$tarX%*%fmod16$coefficients
-scale_mat <- (diag(nrow(listw2mat(sp_weights_16))) - fmod16$lambda*listw2mat(sp_weights_16))
-rev_res <- solve(scale_mat)%*%fmod16$residuals
-moran.test(rev_res, sp_weights_16) # Confirmed spatial correlation in regular residuals
-
-# GLS model
-gls_data <- data.frame(LNP_Percent = fmod16$tary, fmod16$tarX[, -1])
-
-names(gls_data) <- c("LNP_Percent", names(model_df %>% dplyr::select(-c(year, DivisionNm, LNP_Percent))))
-
-gls_formula = paste0("LNP_Percent ~ ", paste0(names(model_df %>% dplyr::select(-c(year, DivisionNm, LNP_Percent))), collapse = " + "))
-
-gls_model <- gls(LNP_Percent ~ AusCitizen + Born_Asia + Born_MidEast + Born_SE_Europe + Born_UK + BornElsewhere + Buddhism + Christianity + CurrentlyStudying + DeFacto + DiffAddress + Distributive + Extractive + Indigenous + Islam + Judaism + ManagerAdminClericalSales + Married + MedianAge + NoReligion + OneParent_House + OtherLanguageHome + SocialServ + Transformative + Education + FamHouseSize + PropertyOwned + RentLoanPrice + Incomes + Unemployment, data = gls_data)
-
 # --------------------------------------------------------------------------------------------------
 
-# OLS: Full models for each year
+## Visualise correlation parameter - only significant in 2001 and 2016
 
-lmod16 <- lm(LNP_Percent ~ ., 
-  data=(model_df %>% filter(year == "2016") %>% dplyr::select(-c(year, DivisionNm))))
+rho_df <- bind_rows(glsmod16$rho_df %>% mutate(year = "2016"), 
+  glsmod13$rho_df %>% mutate(year = "2013"),
+  glsmod10$rho_df %>% mutate(year = "2010"), 
+  glsmod07$rho_df %>% mutate(year = "2007"),
+  glsmod04$rho_df %>% mutate(year = "2004"), 
+  glsmod01$rho_df %>% mutate(year = "2001")) %>% 
+  mutate(upper95 = estimate + 1.96*se, lower95 = estimate - 1.96*se)
 
-lmod13 <- lm(LNP_Percent ~ ., 
-  data=(model_df %>% filter(year == "2013") %>% dplyr::select(-c(year, DivisionNm))))
+rho_df %>% ggplot() + 
+  geom_point(aes(x = year, y = estimate, col = factor(p < 0.05)), size = 3) +
+  geom_linerange(aes(x = year, ymin = lower95, ymax = upper95, col = factor(p < 0.05)), size = 1.5) + 
+  geom_hline(aes(yintercept = 0), alpha = 0.5, size = 1) +
+  scale_color_manual(values = c("grey50", "black"))
 
-lmod10 <- lm(LNP_Percent ~ ., 
-  data=(model_df %>% filter(year == "2010") %>% dplyr::select(-c(year, DivisionNm))))
+## Spatial correlation in GLS residuals - none
 
-lmod07 <- lm(LNP_Percent ~ ., 
-  data=(model_df %>% filter(year == "2007") %>% dplyr::select(-c(year, DivisionNm))))
+moran.test(glsmod16$residuals, sp_weights_16)
+moran.test(glsmod13$residuals, sp_weights_13)
+moran.test(glsmod10$residuals, sp_weights_10)
+moran.test(glsmod07$residuals, sp_weights_07)
+moran.test(glsmod04$residuals, sp_weights_04)
+moran.test(glsmod01$residuals, sp_weights_01)
 
-lmod04 <- lm(LNP_Percent ~ ., 
-  data=(model_df %>% filter(year == "2004") %>% dplyr::select(-c(year, DivisionNm))))
-
-lmod01 <- lm(LNP_Percent ~ ., 
-  data=(model_df %>% filter(year == "2001") %>% dplyr::select(-c(year, DivisionNm))))
-
-# Check for spatial correlation in residuals
-
-moran.test(lmod16$residuals, sp_weights_16)
-moran.test(lmod13$residuals, sp_weights_13)
-moran.test(lmod10$residuals, sp_weights_10)
-moran.test(lmod07$residuals, sp_weights_07)
-moran.test(lmod04$residuals, sp_weights_04)
-moran.test(lmod01$residuals, sp_weights_01)
-
-# Only 2001 and 2016 show evidence of spatial correlation in residuals
